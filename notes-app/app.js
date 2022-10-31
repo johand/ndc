0de0e921a75ab4ebdc2363bc5307e9fcc7e62278
Johan <johand@openmailbox.org>
2022-10-31 19:36:59 -0400

    Adding a note

--- notes-app/app.js
+++ notes-app/app.js
@@ -19,8 +19,7 @@ yargs.command({
     },
   },
   handler: argv => {
-    console.log('Title: ' + argv.title);
-    console.log('Body: ' + argv.body);
+    notes.addNote(argv.title, argv.body);
   },
 });
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,44 @@
+const fs = require('fs');
+
 const getNotes = () => {
   return 'Your notes...';
 };
 
-module.exports = getNotes;
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(note => {
+    return note.title === title;
+  });
+
+  if (!duplicateNotes.length) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+
+    saveNotes(notes);
+    console.log('New note added!');
+  } else {
+    console.log('Note title taken!');
+  }
+};
+
+const saveNotes = notes => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+};
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+};
