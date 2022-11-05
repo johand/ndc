f689fd183ff1cf22e604b73288291cc23963dd0c
Johan <johand@openmailbox.org>
2022-11-05 18:18:43 -0400

    Making http requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,9 @@
-console.log('Starting');
-setTimeout(() => console.log('2 Second Timer'), 2000);
-setTimeout(() => console.log('0 Second Timer'), 0);
-console.log('Stopping');
+const request = require('postman-request');
+
+const url =
+  'http://api.weatherstack.com/current?access_key=my_key&query=37.8267,-122.4233';
+
+request({ url: url }, (_err, res) => {
+  const data = JSON.parse(res.body);
+  console.log(data.current);
+});
