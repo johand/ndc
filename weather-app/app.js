32878e7e525757603051c442ce800db90794c083
Johan <johand@openmailbox.org>
2022-11-05 19:03:28 -0400

    Customizing http requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,11 @@
 const request = require('postman-request');
 
 const url =
-  'http://api.weatherstack.com/current?access_key=my_key&query=37.8267,-122.4233';
+  'http://api.weatherstack.com/current?access_key=my_key&query=37.8267,-122.4233&units=f';
 
-request({ url: url }, (_err, res) => {
-  const data = JSON.parse(res.body);
-  console.log(data.current);
+request({ url: url, json: true }, (_err, res) => {
+  const { temperature, feelslike, weather_descriptions } = res.body.current;
+  console.log(
+    `${weather_descriptions[0]}. It's currently ${temperature} degress out. It feels like ${feelslike} degress out.`,
+  );
 });
