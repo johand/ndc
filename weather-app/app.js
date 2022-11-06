85bc8b3349d472639c12db0dc4a83e8dd4c21e14
Johan <johand@openmailbox.org>
2022-11-05 20:17:54 -0400

    An http request challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -9,3 +9,11 @@ request({ url: url, json: true }, (_err, res) => {
     `${weather_descriptions[0]}. It's currently ${temperature} degress out. It feels like ${feelslike} degress out.`,
   );
 });
+
+const geocodeUrl =
+  'http://api.positionstack.com/v1/forward?access_key=my_key&query=los%20angeles&limit=1';
+
+request({ url: geocodeUrl, json: true }, (_err, res) => {
+  const { label, longitude, latitude } = res.body.data[0];
+  console.log(`City: ${label} Latitude: ${latitude} Longitude: ${longitude}`);
+});
