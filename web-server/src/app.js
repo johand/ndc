776c36f325ef17980837b6d7e9dae46208ad2965
Johan <johand@openmailbox.org>
2022-11-10 21:22:44 -0400

    Dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,29 @@ const express = require('express');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (_req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Foo bar',
+  });
+});
+
+app.get('/about', (_req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'Foo bar',
+  });
+});
+
+app.get('/help', (_req, res) => {
+  res.render('help', {
+    helpText: 'This is some helpful text.',
+  });
+});
 
 app.get('/weather', (_req, res) => {
   res.send({
