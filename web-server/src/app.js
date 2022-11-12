d8e476d2f52c31b147573b9f369ae8d814895433
Johan <johand@openmailbox.org>
2022-11-12 19:30:37 -0400

    Advanced templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -31,6 +34,8 @@ app.get('/about', (_req, res) => {
 app.get('/help', (_req, res) => {
   res.render('help', {
     helpText: 'This is some helpful text.',
+    title: 'Help',
+    name: 'Foo bar',
   });
 });
 
