9de02d39a361c0c00122e41ba7ac021b3a9f0bdc
Johan <johand@openmailbox.org>
2022-11-15 19:34:44 -0400

    Browser http requests with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Client side javascript file is loaded');
+
+fetch('http://localhost:3000/weather?address=boston').then(res => {
+  res.json().then(data => {
+    if (data.error) {
+      return console.log(data.error);
+    }
+
+    console.log(data.location);
+    console.log(data.forecast);
+  });
+});
