cfd4ca90161e948d1fc86e2af15c89b5e145c3c4
Johan <johand@openmailbox.org>
2022-11-15 20:11:26 -0400

    Creating a search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,20 @@
 console.log('Client side javascript file is loaded');
 
-fetch('http://localhost:3000/weather?address=boston').then(res => {
-  res.json().then(data => {
-    if (data.error) {
-      return console.log(data.error);
-    }
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
 
-    console.log(data.location);
-    console.log(data.forecast);
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const location = search.value;
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then(res => {
+    res.json().then(data => {
+      if (data.error) {
+        return console.log(data.error);
+      }
+
+      console.log(data.location);
+      console.log(data.forecast);
+    });
   });
 });
