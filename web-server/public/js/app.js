16ecc95cec25343ea37beb8ecb204f08d6966f15
Johan <johand@openmailbox.org>
2022-11-15 21:16:49 -0400

    Wiring up the user interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,19 +2,24 @@ console.log('Client side javascript file is loaded');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', e => {
   e.preventDefault();
   const location = search.value;
 
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
   fetch(`http://localhost:3000/weather?address=${location}`).then(res => {
     res.json().then(data => {
       if (data.error) {
-        return console.log(data.error);
+        return (messageOne.textContent = data.error);
       }
 
-      console.log(data.location);
-      console.log(data.forecast);
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forecast;
     });
   });
 });
