945c47b0e9c9108e55d182d76238052994289a89
Johan <johand@openmailbox.org>
2022-11-17 23:00:07 -0400

    Inserting documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,22 +1,41 @@
 const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
+const MongoClient = new mongodb.MongoClient(connectionURL);
 const databaseName = 'task-manager';
 
-MongoClient.connect(
-  connectionURL,
-  { useNewUrlParser: true },
-  (error, client) => {
-    if (error) {
-      return console.log('Unable to connect to database');
-    }
+const main = async () => {
+  await MongoClient.connect();
+  const db = MongoClient.db(databaseName);
 
-    const db = client.db(databaseName);
+  // try {
+  //   await db.collection('users').insertOne({ name: 'Foo', age: 50 });
+  // } catch (error) {
+  //   return console.log('Unable to insert user');
+  // }
 
-    db.collection('users').insertOne({
-      name: 'Foo',
-      age: 50,
-    });
-  },
-);
+  // try {
+  //   const result = await db.collection('users').insertMany([
+  //     { Name: 'Bar', age: 25 },
+  //     { Name: 'Biz', age: 27 },
+  //   ]);
+  //
+  //   console.log(result);
+  // } catch (error) {
+  //   return console.log('Unable to insert documents!');
+  // }
+
+  try {
+    const result = await db.collection('tasks').insertMany([
+      { description: 'qwerty', completed: true },
+      { description: 'zxcvbn', completed: false },
+      { description: 'hjkl', completed: true },
+    ]);
+
+    console.log(result);
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+main();
