bef25b54e02c15e3018ce7a438d9ec6990e6b51b
Johan <johand@openmailbox.org>
2022-11-18 18:35:29 -0400

    The ObjectID

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,15 +1,21 @@
-const mongodb = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
-const MongoClient = new mongodb.MongoClient(connectionURL);
+const client = new MongoClient(connectionURL);
 const databaseName = 'task-manager';
 
+const id = new ObjectId();
+console.log(id.id.length);
+console.log(id.toHexString().length);
+
 const main = async () => {
-  await MongoClient.connect();
-  const db = MongoClient.db(databaseName);
+  await client.connect();
+  const db = client.db(databaseName);
 
   // try {
-  //   await db.collection('users').insertOne({ name: 'Foo', age: 50 });
+  //   await db
+  //     .collection('users')
+  //     .insertOne({ name: 'Vikram', age: 26 });
   // } catch (error) {
   //   return console.log('Unable to insert user');
   // }
@@ -25,17 +31,17 @@ const main = async () => {
   //   return console.log('Unable to insert documents!');
   // }
 
-  try {
-    const result = await db.collection('tasks').insertMany([
-      { description: 'qwerty', completed: true },
-      { description: 'zxcvbn', completed: false },
-      { description: 'hjkl', completed: true },
-    ]);
-
-    console.log(result);
-  } catch (error) {
-    return console.log(error);
-  }
+  // try {
+  //   const result = await db.collection('tasks').insertMany([
+  //     { description: 'qwerty', completed: true },
+  //     { description: 'zxcvbn', completed: false },
+  //     { description: 'hjkl', completed: true },
+  //   ]);
+  //
+  //   console.log(result);
+  // } catch (error) {
+  //   return console.log(error);
+  // }
 };
 
 main();
