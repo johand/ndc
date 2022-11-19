d5ac03ccec8ef12b5c5b3ddf83ea4420050176a1
Johan <johand@openmailbox.org>
2022-11-19 19:28:45 -0400

    Data validation and sanitization part I

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,38 +1,58 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
 
 const User = mongoose.model('User', {
   name: {
     type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    },
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number');
+      }
+    },
   },
 });
 
-// const me = new User({ name: 'Foo', age: 50 });
+const me = new User({ name: '  Foo  ', email: 'FOO@FOO.COM  ' });
+
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch(error => {
+    console.log('Error!', error);
+  });
+
+// const Tasks = mongoose.mozdel('Tasks', {
+//   description: {
+//     type: String,
+//   },
+//   completed: {
+//     type: Boolean,
+//   },
+// });
 //
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch(error => {
-//     console.log('Error!', error);
-//   });
-
-const Tasks = mongoose.model('Tasks', {
-  description: {
-    type: String,
-  },
-  completed: {
-    type: Boolean,
-  },
-});
-
-const task = new Tasks({ description: 'qwerty', completed: 5 });
-
-task
-  .save()
-  .then(() => console.log(task))
-  .catch(err => console.log(err));
+// const task = new Tasks({ description: 'qwerty', completed: 5 });
+//
+// task
+//   .save()
+//   .then(() => console.log(task))
+//   .catch(err => console.log(err));
