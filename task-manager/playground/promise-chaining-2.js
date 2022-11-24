8bd971ca1a32afd2e375f948841fdf1c57207f6c
Johan <johand@openmailbox.org>
2022-11-23 22:12:41 -0400

    Async/Await part II

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,10 +1,20 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('637d771555e041674c62556f')
-  .then(task => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
-  .then(result => console.log(result))
+// Task.findByIdAndDelete('637d771555e041674c62556f')
+//   .then(task => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then(result => console.log(result))
+//   .catch(err => console.log(err));
+
+const deleteTaskAndCount = async id => {
+  const task = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return { task, count };
+};
+
+deleteTaskAndCount('637ed206a838ebe6df399d5b')
+  .then(res => console.log(res))
   .catch(err => console.log(err));
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,10 +1,20 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user');
 
-User.findByIdAndUpdate('637ab98af9160bda5ff3e970', { age: 1 })
-  .then(user => {
-    console.log(user);
-    return User.countDocuments({ age: 1 });
-  })
+// User.findByIdAndUpdate('637ab98af9160bda5ff3e970', { age: 1 })
+//   .then(user => {
+//     console.log(user);
+//     return User.countDocuments({ age: 1 });
+//   })
+//   .then(result => console.log(result))
+//   .catch(err => console.log(err));
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return { user, count };
+};
+
+updateAgeAndCount('637ab98af9160bda5ff3e970', 2)
   .then(result => console.log(result))
   .catch(err => console.log(err));
