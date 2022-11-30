8a43aa1ce3cc01afaeed87b1c4d1d1ba5d52f451
Johan <johand@openmailbox.org>
2022-11-29 22:12:21 -0400

    Logging Out

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -31,6 +31,30 @@ router.post('/users/login', async (req, res) => {
   }
 });
 
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => {
+      return token.token !== req.token;
+    });
+
+    await req.user.save();
+    res.send();
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+
+    await req.user.save();
+    res.send();
+  } catch (err) {
+    res.status(500).send;
+  }
+});
+
 router.get('/users/me', auth, async (req, res) => {
   res.send(req.user);
 });
