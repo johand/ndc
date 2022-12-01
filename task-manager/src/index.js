3c1fd8a7260aad747a2e60259d0b28b259c8ac59
Johan <johand@openmailbox.org>
2022-11-30 21:48:16 -0400

    Hiding private data

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -23,18 +23,3 @@ app.use(userRouter);
 app.use(taskRouter);
 
 app.listen(port, () => console.log(`Server is up on port ${port}`));
-
-const jwt = require('jsonwebtoken');
-
-const myFunction = async () => {
-  const token = jwt.sign({ _id: 'abc123' }, 'zxcvbn', {
-    expiresIn: '7 days',
-  });
-
-  console.log(token);
-
-  const data = jwt.verify(token, 'zxcvbn');
-  console.log(data);
-};
-
-myFunction();
