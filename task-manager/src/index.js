74f30c56b1751c29bb8398e668b2bef042a377d6
Johan <johand@openmailbox.org>
2022-12-02 20:14:29 -0400

    The user/task relationship

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000;
 // });
 
 // app.use((req, res, next) => {
-//   res.status(503).send('Site is currently down, Check back soon!');
+//   res.status(503).send('Site is currently  down, Check back soon!');
 // });
 
 app.use(express.json());
@@ -23,3 +23,18 @@ app.use(userRouter);
 app.use(taskRouter);
 
 app.listen(port, () => console.log(`Server is up on port ${port}`));
+
+const Task = require('./models/task');
+const User = require('./models/user');
+
+const main = async () => {
+  // const task = await Task.findById('638a8a4d04466e8e9194983d');
+  // await task.populate('owner');
+  // console.log(task.owner);
+
+  const user = await User.findById('63894d770cb9bc6c7f938450');
+  await user.populate('tasks');
+  console.log(user.tasks);
+};
+
+main();
