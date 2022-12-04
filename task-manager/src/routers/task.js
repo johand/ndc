b64198f650947ef99e370c6adedf223b86b3d80d
Johan <johand@openmailbox.org>
2022-12-03 21:12:04 -0400

    Filtering data

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -17,13 +17,23 @@ router.post('/tasks', auth, async (req, res) => {
   }
 });
 
+// GET /tasks?completed=true
 router.get('/tasks', auth, async (req, res) => {
+  const match = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
+
   try {
     // const tasks = await Task.find({
     //   owner: req.user._id,
     // });
 
-    await req.user.populate('tasks');
+    await req.user.populate({
+      path: 'tasks',
+      match,
+    });
 
     res.send(req.user.tasks);
   } catch (err) {
