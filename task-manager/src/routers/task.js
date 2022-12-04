ee39a964e03d40d7f0ba5628117a4c049b0c9129
Johan <johand@openmailbox.org>
2022-12-03 22:38:18 -0400

    Sorting data

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -21,9 +21,15 @@ router.post('/tasks', auth, async (req, res) => {
 // GET /tasks?limit
 router.get('/tasks', auth, async (req, res) => {
   const match = {};
+  const sort = {};
 
   if (req.query.completed) {
     match.completed = req.query.completed === 'true';
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':');
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
   }
 
   try {
@@ -37,6 +43,7 @@ router.get('/tasks', auth, async (req, res) => {
       options: {
         limit: parseInt(req.query.limit),
         skip: parseInt(req.query.skip),
+        sort,
       },
     });
 
