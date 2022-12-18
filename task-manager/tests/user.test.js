1176fbd51a35dcde73641f4e9bb9ead43cdf8ea9
Johan <johand@openmailbox.org>
2022-12-17 23:32:09 -0400

    Jest setup and teardown

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,5 +1,22 @@
+const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/models/user');
+
+const userOne = {
+  name: 'testUser',
+  email: 'testUser@example.com',
+  password: '1234567',
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
 
 test('Should sign up a new user', async () => {
   await request(app)
@@ -10,4 +27,24 @@ test('Should sign up a new user', async () => {
       password: '1234567',
     })
     .expect(201);
+});
+
+test('Should login existing user', async () => {
+  await request(app)
+    .post('/users/login')
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200);
+});
+
+test('Should not login nonexistent user', async () => {
+  await request(app)
+    .post('/users/login')
+    .send({
+      email: userOne.email,
+      password: 'wrongPassword',
+    })
+    .expect(400);
 });
