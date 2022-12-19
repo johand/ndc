5ddbc71adb6c45b08792e972585620775b1af6a6
Johan <johand@openmailbox.org>
2022-12-18 23:02:21 -0400

    Testing with task data

--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -1,7 +1,12 @@
 const request = require('supertest');
 const app = require('../src/app');
 const Task = require('../src/models/task');
-const { userOne, userOneId, setupDatabase } = require('../tests/fixtures/db');
+const {
+  userOne,
+  userTwo,
+  taskOne,
+  setupDatabase,
+} = require('../tests/fixtures/db');
 
 beforeEach(setupDatabase);
 
@@ -16,4 +21,25 @@ test('Should create task for user', async () => {
 
   expect(task).not.toBeNull();
   expect(task.completed).toBe(false);
+});
+
+test('Should fetch user tasks', async () => {
+  const response = await request(app)
+    .get('/tasks')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  expect(response.body.length).toEqual(2);
+});
+
+test('Should not delete other users tasks', async () => {
+  await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404);
+
+  const task = Task.findById(taskOne._id);
+  expect(task).not.toBeNull();
 });
