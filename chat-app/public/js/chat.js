d913a1c986a5f24c8fb5a20c9c0c8033e9bcaa1b
Johan <johand@openmailbox.org>
2022-12-29 23:04:44 -0400

    Rendering messages

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -5,9 +5,19 @@ const $messageForm = document.querySelector('#msg-form');
 const $messageFormInput = $messageForm.querySelector('input');
 const $messageFormButton = $messageForm.querySelector('button');
 const $sendLocationButton = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages');
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
 
 socket.on('message', msg => {
   console.log(msg);
+
+  const html = Mustache.render(messageTemplate, {
+    message: msg,
+  });
+
+  $messages.insertAdjacentHTML('beforeend', html);
 });
 
 $messageForm.addEventListener('submit', e => {
