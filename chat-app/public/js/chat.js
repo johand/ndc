9fa05ebfa044092a6cafa80496c62a552e6d6d20
Johan <johand@openmailbox.org>
2022-12-29 21:45:01 -0400

    Form and button states

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,15 +1,29 @@
 const socket = io();
 
+// Elements
+const $messageForm = document.querySelector('#msg-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+
 socket.on('message', msg => {
   console.log(msg);
 });
 
-document.querySelector('#msg-form').addEventListener('submit', e => {
+$messageForm.addEventListener('submit', e => {
   e.preventDefault();
+
+  // disabled
+  $messageFormButton.setAttribute('disabled', 'disabled');
 
   const message = e.target.elements.message.value;
 
   socket.emit('sendMessage', message, error => {
+    // enable
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
+
     if (error) {
       return console.log(error);
     }
@@ -18,7 +32,9 @@ document.querySelector('#msg-form').addEventListener('submit', e => {
   });
 });
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
+  $sendLocationButton.setAttribute('disabled', 'disabled');
+
   if (!navigator.geolocation) {
     return alert('Geolocation is not supported by your browser.');
   }
@@ -27,6 +43,7 @@ document.querySelector('#send-location').addEventListener('click', () => {
     const { latitude, longitude } = position.coords;
 
     socket.emit('sendLocation', { latitude, longitude }, () => {
+      $sendLocationButton.removeAttribute('disabled');
       console.log('Location shared!');
     });
   });
